9834d176f1fc9e1e998a234f1a859d38a77f1eba
Agustina Barrena <abarrena@udesa.edu.ar>
2025-03-09 18:27:56 -0300

    crear matriz e imprimir indices

--- ejercicio1/main.cpp
+++ ejercicio1/main.cpp
@@ -1,32 +1,34 @@
-int* crear_matriz(int n){
-    if(n <= 1){
-        int filas = n;
-        int columnas = n;
-        for(int i = 0; i < filas; i++){
-            for(int j = 0; j < columnas; j++){
-                matriz[i][j] = i;
-            }
+#include <iostream>
+using namespace std;
+#include <vector>
+
+vector<vector<int>> crear_matriz(int n){
+    if(n < 1) return {};
+    
+    vector<vector<int>> matriz(n, vector<int>(n));
+    int valor_inicial = 1;
+    for(int i= 0; i< n; i++){
+        for(int j = 0; j < n; j++){
+            matriz[i][j] = valor_inicial++;
         }
-        return matriz;
     }
-    else{
-        return NULL;
+    return matriz;
+}
+void imprimir_coordenadas(const vector<vector<int>>& matriz) {
+    int n = matriz.size();
+    int pos_totales = n * n;
+
+    for(int i = pos_totales; i > 0; i--){
+        int fila = (i - 1) / n;
+        int col = i - (fila * n) - 1;
+        cout << "M[" << fila << "][" << col << "] = " << matriz[fila][col] << endl;
     }
+    
 }
 
-int main(){
-    int n = 5;
-    int* matriz = crear_matriz(n);
-    if(matriz != NULL){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                cout << matriz[i][j] << " ";
-            }
-            cout << endl;
-        }
-    }
-    else{
-        cout << "Error: n debe ser menor o igual a 1" << endl;
-    }
+int main() {
+    int n = 2;
+    vector<vector<int>> matriz = crear_matriz(n);
+    imprimir_coordenadas(matriz);
     return 0;
 }
