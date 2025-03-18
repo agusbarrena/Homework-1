114a58a3b2bf5ded4b369e6cd83145c97578e9c0
Agustina Barrena <abarrena@udesa.edu.ar>
2025-03-18 19:52:53 -0300

    Cambios en funciones.h

--- ejercicio_1/funciones.h
+++ ejercicio_1/funciones.h
@@ -1,5 +1,9 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
 #include <vector>
 
 std::vector<std::vector<int>>crear_matriz(int n);
 
 void imprimir_coordenadas(const std::vector<std::vector<int>>& matriz);
+
+#endif // FUNCIONES_H
